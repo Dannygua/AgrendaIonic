6bc62e2b8ac9ebf1c39e4d8090d68c49c1149231
DannyGua <Danny.guanuna@epn.edu.ec>
2020-12-09 00:10:18 -0500

    Registro

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDanIi4wL57e2PytIUJx9hlqXwzA16xpkg",
+    authDomain: "nuevo-poryecto11111111.firebaseapp.com",
+    projectId: "nuevo-poryecto11111111",
+    storageBucket: "nuevo-poryecto11111111.appspot.com",
+    messagingSenderId: "691264158506",
+    appId: "1:691264158506:web:9b2f136d46aa99a20d3c53",
+    measurementId: "G-G6HED2MPNP"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDanIi4wL57e2PytIUJx9hlqXwzA16xpkg",
+    authDomain: "nuevo-poryecto11111111.firebaseapp.com",
+    projectId: "nuevo-poryecto11111111",
+    storageBucket: "nuevo-poryecto11111111.appspot.com",
+    messagingSenderId: "691264158506",
+    appId: "1:691264158506:web:9b2f136d46aa99a20d3c53",
+    measurementId: "G-G6HED2MPNP"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
